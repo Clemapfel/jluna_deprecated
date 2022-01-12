318eca11ae54509b1cd76155b1a9fb8e27e86b67
clem <clemens.cords@aol.com>
2022-01-12 22:11:25 +0100

    added typedef header

---
+++ .src/typedefs.hpp
@@ -0,0 +1,12 @@
+// 
+// Copyright 2022 Clemens Cords
+// Created on 12.01.22 by clem (mail@clemens-cords.com)
+//
+
+#pragma once
+
+namespace jluna
+{
+    /// typedefs for prettier code
+
+}
---
+++ include/typedefs.hpp
@@ -0,0 +1,11 @@
+// 
+// Copyright 2022 Clemens Cords
+// Created on 12.01.22 by clem (mail@clemens-cords.com)
+//
+
+#pragma once
+
+namespace jluna
+{
+
+}
